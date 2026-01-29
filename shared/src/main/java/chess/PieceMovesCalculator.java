05a49f712b16afb6d1c7ac248ebf14faa91a2cd0
madalynkeyes <madalynkeyes@gmail.com>
2026-01-29 16:33:01 -0700

    impl(p0) added documentation to PieceMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -5,6 +5,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Parent class for all the different pieces and their legal moves.
+ * Contains the main methods that are implemented differently by each subclass.
+ */
 public class PieceMovesCalculator {
     public final ChessBoard board;
     public final ChessPosition position;
@@ -17,14 +21,25 @@ public class PieceMovesCalculator {
         piece = board.getPiece(position);
     }
 
+    /**
+     * Getter for movesList (list of possible moves)
+     * @return movesList
+     */
     public List<ChessMove> getMovesList() {
         return movesList;
     }
 
+    /**
+     * Gives us a list of all possible moves for a specific piece.
+     * @return movesList
+     */
     public Collection<ChessMove>getPossibleMoves(){
         return movesList;
     }
 
+    /**
+     * If possible to move in specified direction, add that move to movesList.
+     */
     public void addMoveIfPossible(String direction){
         ChessPosition endPosition = getDesiredPosition(position,direction);
         if (endPosition!=null){
@@ -34,12 +49,23 @@ public class PieceMovesCalculator {
         }
     }
 
+    /**
+     * Check if enemy piece in desired position and whether specified piece can capture enemy or move.
+     * @param endPosition desired position of piece
+     * @return boolean of whether piece can move to end position.
+     */
     public boolean canCaptureOrMove(ChessPosition endPosition) {
         if (board.getPiece(endPosition)!=null){
             return board.getPiece(endPosition).getTeamColor() != piece.getTeamColor();
         } return true;
     }
 
+    /**
+     * Get the position of the square in specified direction from current location of piece.
+     * @param position current location/square of the piece
+     * @param direction specified direction the piece wants to travel
+     * @return desired position
+     */
     public ChessPosition getDesiredPosition(ChessPosition position, String direction) {
         if (Objects.equals(direction, "north") && position.getRow()!=8){
             return new ChessPosition(position.getRow()+1, position.getColumn());
