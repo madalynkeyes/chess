3f0e52e62b36b136dd05a8ca1eda5632b8091fc9
madalynkeyes <madalynkeyes@gmail.com>
2026-02-13 13:14:14 -0700

    impl(p3) getting started

---
+++ server/src/main/java/server/Main.java
@@ -0,0 +1,20 @@
+package server;//package server;
+//
+//import chess.*;
+//
+//public class ServerMain {
+//    public static void main(String[] args) {
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+//    }
+//}
+import server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
+    }
+}
